377d99dd38f46d0185491b1bd7570934c2dbc587
inchoriya <hic0710@naver.com>
2021-12-14 18:33:27 +0900

    IntelliJ GitHub Test

---
+++ src/main/java/com/icia/githubtest13/GitHubTest13Application.java
@@ -0,0 +1,13 @@
+package com.icia.githubtest13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubTest13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubTest13Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/icia/githubtest13/GitHubTest13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icia.githubtest13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubTest13ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
